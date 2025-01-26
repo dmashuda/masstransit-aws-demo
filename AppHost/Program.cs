68456236abed99436cd82f40622d8f51ec415ace
Daniel Mashuda <danmashuda@gmail.com>
2025-01-26 17:24:53 -0500

    Add aws config

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,3 +1,31 @@
+using Amazon.CloudFormation;
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+
+var localStackUri = new Uri("http://localhost:4566");
+
+// var cfConfig = new AmazonCloudFormationConfig
+// {
+//     ServiceURL = localStackUri.ToString()
+// };
+// var cfClient = new AmazonCloudFormationClient(cfConfig);
+
+var localStack = builder.AddContainer("localstack", "localstack/localstack")
+    .WithEndpoint(port: localStackUri.Port, targetPort: 4566, scheme: "http")
+    .WithEnvironment("DEBUG", "1");
+
+// var awsResources = builder.AddAWSCloudFormationTemplate("LocalStackExample-Stack", "aws-resources.template");
+// awsResources.Resource.CloudFormationClient = cfClient;
+
+
+builder.AddProject<Backend>("Backend")
+    .WaitFor(localStack)
+    .WithEnvironment($"AWS_ENDPOINT_URL", localStackUri.ToString())
+    .WithEnvironment($"AWS_SECRET_KEY", "default")
+    .WithEnvironment($"AWS_ACCESS_KEY", "default");
+
+;
 
 builder.Build().Run();
