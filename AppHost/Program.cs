6865ca05191e7a1d301f4a69200e1c494ee9d725
Daniel Mashuda <danmashuda@gmail.com>
2025-01-26 20:36:33 -0500

    Improve container lifetime

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -15,7 +15,8 @@ var localStackUri = new Uri("http://localhost:4566");
 var localStack = builder.AddContainer("localstack", "localstack/localstack")
     .WithEndpoint(port: localStackUri.Port, targetPort: 4566, scheme: "http")
     .WithEnvironment("DEBUG", "1")
-    .WithHttpHealthCheck("/_localstack/health");
+    .WithHttpHealthCheck("/_localstack/health")
+    .WithLifetime(ContainerLifetime.Persistent);
 
 // var awsResources = builder.AddAWSCloudFormationTemplate("LocalStackExample-Stack", "aws-resources.template");
 // awsResources.Resource.CloudFormationClient = cfClient;
