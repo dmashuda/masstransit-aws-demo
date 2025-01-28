8ef790f36f2bcfa2bd5d7f25489f72d1ba231988
Daniel Mashuda <danmashuda@gmail.com>
2025-01-27 19:50:50 -0500

    Update readme

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,26 +1,19 @@
-using Amazon.CloudFormation;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-
 var localStackUri = new Uri("http://localhost:4566");
 
-// var cfConfig = new AmazonCloudFormationConfig
-// {
-//     ServiceURL = localStackUri.ToString()
-// };
-// var cfClient = new AmazonCloudFormationClient(cfConfig);
 
+// use localstack so you don't need to set up an aws account
 var localStack = builder.AddContainer("localstack", "localstack/localstack")
     .WithEndpoint(port: localStackUri.Port, targetPort: 4566, scheme: "http")
     .WithEnvironment("DEBUG", "1")
-    .WithHttpHealthCheck("/_localstack/health")
-    .WithLifetime(ContainerLifetime.Persistent);
+    .WithHttpHealthCheck("/_localstack/health") 
+    .WithLifetime(ContainerLifetime.Persistent); // Use a persistent lifetime so the container does not restart 
 
-// var awsResources = builder.AddAWSCloudFormationTemplate("LocalStackExample-Stack", "aws-resources.template");
-// awsResources.Resource.CloudFormationClient = cfClient;
 
+// Setup job will create the sqs/sns resources then exit
 var setup = builder.AddProject<Setup>("Setup")
     .WaitFor(localStack)
     .WithEnvironment($"AWS_ENDPOINT_URL", localStackUri.ToString())
@@ -30,20 +23,17 @@ var setup = builder.AddProject<Setup>("Setup")
 
 builder.AddProject<Backend>("Backend")
     .WaitFor(localStack)
-    .WaitForCompletion(setup)
+    .WaitForCompletion(setup) // wait for setup job to complete before starting 
     .WithEnvironment($"AWS_ENDPOINT_URL", localStackUri.ToString())
     .WithEnvironment($"AWS_SECRET_KEY", "default")
     .WithEnvironment($"AWS_ACCESS_KEY", "default");
 
 builder.AddProject<Api>("Api")
     .WaitFor(localStack)
-    .WaitForCompletion(setup)
+    .WaitForCompletion(setup) // wait for setup job to complete before starting 
     .WithEnvironment($"AWS_ENDPOINT_URL", localStackUri.ToString())
     .WithEnvironment($"AWS_SECRET_KEY", "default")
     .WithEnvironment($"AWS_ACCESS_KEY", "default");
 
-
-
-;
 
 builder.Build().Run();
--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# masstransit-aws-demo
+# MassTransit AWS Demo
+
+This repository demonstrates the use of MassTransit with AWS SQS Transport+localstack with Aspire. 
+The project is built using C# and showcases how to integrate Aspire, MassTransit, and AWS components for building distributed applications.
+
+## Features
+
+- Runs 100% local (no aws account required)
+- Topography Creation Seperated from Api/Backend Worker Applications
+- Aspire app host
+  - Distributed tracing between Api/Backend Worker Applications
+
+## Prerequisites
+
+- .NET 9.0 SDK or later [official .NET website](https://dotnet.microsoft.com/download).
+- Docker or Podman installed
+
+## Getting Started
+
+### Clone the Repository
+
+```sh
+git clone https://github.com/dmashuda/masstransit-aws-demo.git
+cd masstransit-aws-demo
+```
+
+### Install Dependencies
+
+Ensure you have the .NET SDK installed. You can download it from the 
+
+Running the application also requires docker or podman because 
+
+
+### Running the Application
+
+To launch the application, use the following command:
+
+```sh
+dotnet run --project AppHost 
+```
+
+To send a request to the api run [Api/Api.http](Api/Api.http)
+
+
+## Project Structure
+- `Api/` Api web application 
+- `AppHost/` Aspire Apphost
+- `Backend/` Background worker application that consumes messages 
+- `Components/` Class Library For Consumers
+- `Contracts/` Class Library for Message Contracts
+- `ServiceDefaults/` Aspire ServiceDefaults
+- `Setup/` Application that creates Bus Topography (the sqs/sns resources)
+
+## Contributing
+
+This is a demo app, so improvements are welcome via PR
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
